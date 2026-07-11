72f2158a72a5004a5a6bfcc80f08b4cb15d105a7
Thailon Dorneles <thailon74@gmail.com>
2026-07-11 14:54:29 +0400

    Refectoring on Repositories

---
+++ myfinance-web-dotnet-domain/Entities/Base/EntityBase.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace myfinance_web_dotnet_domain.Entities.Base
+{
+    public class EntityBase
+    {
+        [Key]
+        public int? Id { get; set; }
+    }
+}
